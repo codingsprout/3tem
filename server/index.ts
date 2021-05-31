a9fe6ed75ccce762da0f00b9543c41046c9bdd89
Alan Lee <alanlee@alans-mbp.myfiosgateway.com>
2021-05-31 16:33:36 -0400

    first commit

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
